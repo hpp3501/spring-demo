b20f62feebb3401172c07c6ad837c140f51fcca8
hpp3501 <hpp3501@163.com>
2020-10-29 16:32:07 +0800

    提交事务管理

---
+++ spring-study/spring-11-transcation/src/main/java/com/kuang/mapper/UserMapperImpl.java
@@ -0,0 +1,23 @@
+package com.kuang.mapper;
+
+import com.kuang.pojo.User;
+import org.mybatis.spring.support.SqlSessionDaoSupport;
+
+import java.util.List;
+
+public class UserMapperImpl extends SqlSessionDaoSupport implements UserMapper {
+    public List<User> selectUser() {
+        User user = new User(10,"李刚", "12123");
+        this.addUser(user);
+        this.deleteUser(10);
+        return getSqlSession().getMapper(UserMapper.class).selectUser();
+    }
+
+    public int addUser(User user) {
+        return getSqlSession().getMapper(UserMapper.class).addUser(user);
+    }
+
+    public int deleteUser(int id) {
+        return getSqlSession().getMapper(UserMapper.class).deleteUser(id);
+    }
+}
